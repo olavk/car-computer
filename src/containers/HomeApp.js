220edde3e63f4c1c214f7a92a54ed29f2de6ee16
Olav <olav.kokovkin@gmail.com>
2016-06-19 22:11:34 +0300

    init

---
+++ src/containers/HomeApp.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class CounterApp extends Component {
+  render() {
+    return (
+      <div>You're home!</div>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import configureStore from './store/configureStore';
+import Root from './containers/Root';
+const store = configureStore();
+
+render(
+  <AppContainer>
+    <Root store={store}/>
+  </AppContainer>,
+  document.getElementById('root')
+);
+
+if (module.hot) {
+  module.hot.accept('./containers/Root', () => {
+    render(
+      <AppContainer>
+        {React.createElement(require('./containers/Root').default, {store})}
+      </AppContainer>,
+      document.getElementById('root')
+    );
+  });
+}
